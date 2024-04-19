7fa8af15d76a773d107d5bceb750a50a690a61fe
Santel1 <126976920+Santel1@users.noreply.github.com>
2024-04-19 15:03:54 +0200

    Updates

--- static/js/801.83630a58.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[801],{3801:(n,p,s)=>{s.r(p),s.d(p,{default:()=>x});var e,t=s(5043),a=s(9456),d=s(4610),c=s(374),i=s(7528);const o=s(197).Ay.div(e||(e=(0,i.A)(["\n  padding: 0px 128px 100px 128px;\n"])));var r=s(1993),u=s(579);const x=()=>{const n=(0,a.d4)(c.Vf),[p,s]=(0,t.useState)(!1);return(0,u.jsxs)(o,{children:[(0,u.jsx)(r.A,{}),(0,u.jsx)(d.A,{nannies:n,appointmentModal:p,setAppointmentModal:s})]})}}}]);
-//# sourceMappingURL=801.83630a58.chunk.js.map
